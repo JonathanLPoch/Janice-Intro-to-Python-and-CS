725e052ab7dcd7ff90624b0c66610bb50545be61
Janice Lee <lanicejee@172-16-78-247.DYNAPOOL.NYU.EDU>
2018-02-11 15:58:25 -0500

    added some functions

---
+++ week 1/week 1.py
@@ -0,0 +1,24 @@
+# print("Pear")
+# print()
+# print("Peach Grapefruit")
+# print()
+# print("Orange Pineapple Grape")
+# def printJ():
+#     print("*****")
+#     print("   * ")
+#     print("   * ")
+#     print("*  * ")
+#     print(" **  ")
+# printJ()
+# def fivePlusFive():
+#     print(5 + 5)
+# def fivePlusSeven():
+#     print(5 + 7)
+# fivePlusFive()
+# fivePlusSeven()
+
+def sumOfTwoNumbers(a,b):
+    print(a+b)
+sumOfTwoNumbers(10,20)
+sumOfTwoNumbers(30,40)
+sumOfTwoNumbers("janice","lee")
